d9aec00a3c33a09348035fa6c886d09be689cccb
lingyu <lingyu@byai.com>
2024-03-20 16:52:51 +0800

    chore: adjustment of document catalogs

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Farm-react-template
 
-开箱即用的Farm-react开发模版
+开箱即用的Farm-react开发模版.
+
+> 集成了开发常用的功能,如路由,代码格式化,commit约束等
 
 
 ## 注意事项
@@ -10,6 +12,7 @@
 
 ## 集成内容
 - [Farm](https://farm-fe.github.io/zh/docs/quick-start): Rust开发的基础框架,提供了一套完整的开发体验
+- [vite-plugin-pages](https://github.com/hannoeru/vite-plugin-pages?tab=readme-ov-file#react-1): 提供约定式路由功能
 - [Biome](https://biomejs.dev/zh-cn/reference/configuration/#javascriptformatterjsxquotestyle): Rust开发的lint和format工具,提升极致的开发体验
-- husky&commitlint: 提供了commit规范和代码提交的检查
-- react-router: 提供了路由的功能
+- `husky`&`commitlint`: 提供了`commit`规范和代码提交的检查
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import routes from '~react-pages';
 import './index.css';
-import { router } from './router/routers';
+
 const container = document.querySelector('#root');
 const root = createRoot(container);
 
-root.render(<RouterProvider router={router} />);
+function App() {
+  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
+}
+
+root.render(
+  <Router>
+    <App />
+  </Router>,
+);
