eaea4ae5c8150592c641f92a47a96f4f43e5e1db
callqh <528851597@qq.com>
2024-05-08 21:20:09 +0800

    fix: modify animate effects

--- src/layout/animate.tsx
+++ src/layout/animate.tsx
@@ -1,28 +1,39 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Animate = ({ children }) => {
-  const [isOpen, setOpen] = useState(false);
-
-  useEffect(() => {
-    setOpen(!isOpen);
-  }, [location.pathname]);
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.5,
+      },
+    },
+  };
 
   return (
     <AnimatePresence>
       <motion.div
         layout
         key="content"
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 0.5,
-          ease: [0, 0.71, 0.2, 1.01],
-        }}
-        style={{ minHeight: isOpen ? '100px' : '500px' }}
-        onClick={() => setOpen(!isOpen)}
+        initial={{ x: -200 }}
+        animate={{ x: 0 }}
+        exit={{ x: 2000 }}
+        transition={{ duration: 0.5 }}
       >
-        {children}
+        <motion.div
+          key={location.pathname}
+          variants={container}
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            duration: 0.3,
+            ease: 'easeIn',
+          }}
+        >
+          {children}
+        </motion.div>
       </motion.div>
     </AnimatePresence>
   );
