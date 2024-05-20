4ab4f9c97594b1522a998feb87cad8637f598c28
callqh <528851597@qq.com>
2024-05-20 15:51:47 +0800

    chore: update dep

--- src/pages/login/RegisterTabs.tsx
+++ src/pages/login/RegisterTabs.tsx
@@ -1,21 +1,20 @@
 import { Tabs } from '@arco-design/web-react';
 import React from 'react';
-import { REGISTER_TYPE, TAB_LIST } from './config'
+import { REGISTER_TYPE, TAB_LIST } from './config';
 
-const TabPane = Tabs.TabPane
+const TabPane = Tabs.TabPane;
 
-const RegisterTabs = ({onTabChange}) => {
-  
+const RegisterTabs = ({ onTabChange }) => {
   const handleTab = (tabVal) => {
-    onTabChange(tabVal)
-  }
+    onTabChange(tabVal);
+  };
   return (
     <Tabs defaultActiveTab={REGISTER_TYPE.EMAIL} onChange={handleTab} className="tab">
-      { TAB_LIST.map(tab => {
-        return <TabPane key={tab.value} title={tab.tabTitle}></TabPane>
+      {TAB_LIST.map((tab) => {
+        return <TabPane key={tab.value} title={tab.tabTitle} />;
       })}
     </Tabs>
-  )
-}
+  );
+};
 
 export default RegisterTabs;
