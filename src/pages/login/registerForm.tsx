7c26e3fdd3e8d4c1e8c50d1fac3f832957651ae2
callqh <528851597@qq.com>
2024-05-20 19:45:28 +0800

    feat: format

--- src/pages/login/registerForm.tsx
+++ src/pages/login/registerForm.tsx
@@ -1,12 +1,12 @@
 import { TOKEN_KEY } from '@/router/const';
 import { setCookie } from '@/utils/cookie';
+import { validateEmail, validatePhone } from '@/utils/validate';
+import { Button, Form, Input, Message } from '@arco-design/web-react';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { Button, Form, Input, Message } from '@arco-design/web-react';
 import RegisterTabs from './RegisterTabs';
-import StrengthCheckInput from './strengthCheck';
 import { PLACEHOLDER, REGISTER_TYPE } from './config';
-import { validateEmail, validatePhone } from '@/utils/validate';
+import StrengthCheckInput from './strengthCheck';
 
 const FormItem = Form.Item;
 
@@ -32,20 +32,20 @@ const RegisterForm = () => {
 
   // 账号校验
   const accountValidator = (value, callback) => {
-    if(REGISTER_TYPE.EMAIL === tabVal) {
+    if (REGISTER_TYPE.EMAIL === tabVal) {
       if (!validateEmail(value)) return callback('邮箱格式有误');
       return callback();
     }
-    if(REGISTER_TYPE.PHONE === tabVal) {
+    if (REGISTER_TYPE.PHONE === tabVal) {
       if (!validatePhone(value)) return callback('手机号格式有误');
       return callback();
     }
     // 默认用户名三位字符
-    if(REGISTER_TYPE.USER_NAME === tabVal) {
+    if (REGISTER_TYPE.USER_NAME === tabVal) {
       if (value.length < 3) return callback('用户名最少三位字符');
       return callback();
     }
-  }
+  };
 
   // 确认密码校验
   const confirmPasswordValidator = (value, callback) => {
@@ -53,7 +53,7 @@ const RegisterForm = () => {
       return callback('两次输入的密码不一致');
     }
     return callback();
-  }
+  };
 
   // 切换tab选择注册方式
   const handleTabChange = (val) => {
@@ -63,22 +63,18 @@ const RegisterForm = () => {
     form.resetFields();
     // 更新placeholder
     setPlaceholder(PLACEHOLDER[val]);
-  }
+  };
   return (
     <div className="form register-form">
       {/* Form wrapperCol 默认19 */}
       <Form form={form} onSubmit={handleRegister} className="w-1/2">
         <FormItem>
-          <RegisterTabs onTabChange={handleTabChange} />        
+          <RegisterTabs onTabChange={handleTabChange} />
         </FormItem>
-        <FormItem field="username"
-          rules={[
-            { required: true, message: placeholder },
-            { validator:  accountValidator },
-          ]}>
+        <FormItem field="username" rules={[{ required: true, message: placeholder }, { validator: accountValidator }]}>
           <Input
             normalizeTrigger={['onBlur']}
-            normalize={v => v ? v.trim() : v}
+            normalize={(v) => (v ? v.trim() : v)}
             placeholder={placeholder}
             style={{ width: '100%' }}
           />
@@ -86,20 +82,18 @@ const RegisterForm = () => {
         <FormItem field="password" rules={[{ required: true, message: '请输入密码' }]}>
           <Input.Password
             normalizeTrigger={['onBlur']}
-            normalize={v => v ? v.trim() : v}
+            normalize={(v) => (v ? v.trim() : v)}
             onChange={(val) => setPassword(val)}
             placeholder="请输入密码"
           />
         </FormItem>
         <FormItem
           field="confirmPassword"
-          rules={[
-            { required: true, message: '请确认密码' },
-            { validator: confirmPasswordValidator }
-          ]}>
+          rules={[{ required: true, message: '请确认密码' }, { validator: confirmPasswordValidator }]}
+        >
           <Input.Password
             normalizeTrigger={['onBlur']}
-            normalize={v => v ? v.trim() : v}
+            normalize={(v) => (v ? v.trim() : v)}
             placeholder="请确认密码"
           />
         </FormItem>
@@ -107,7 +101,9 @@ const RegisterForm = () => {
           <StrengthCheckInput password={password} />
         </FormItem>
         <FormItem>
-          <Button type="primary" htmlType="submit" className="register-btn">注册</Button>
+          <Button type="primary" htmlType="submit" className="register-btn">
+            注册
+          </Button>
         </FormItem>
       </Form>
     </div>
--- src/utils/commonFn.ts
+++ src/utils/commonFn.ts
@@ -1,14 +1,14 @@
 /**
  * 数组根据key value转对象方法
  * [{key: 'key1', value: 'value1'}] => { key1: value1 }
- * @param arr 
- * @param key 
- * @param value 
- * @returns 
+ * @param arr
+ * @param key
+ * @param value
+ * @returns
  */
 export const formatterArrToObj = (arr, key, value) => {
   return arr.reduce((result, item) => {
     result[item[key]] = item[value];
     return result;
   }, {});
-}
+};
